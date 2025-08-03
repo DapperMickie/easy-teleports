84ab94363a747c696688b03e6e97d69ffe2e8e8f
40 Cakes <16377135+40Cakes@users.noreply.github.com>
2025-08-03 14:21:30 +1000

    Fix diary cape options + fix not working with being equipped

--- src/main/java/com/duckblade/osrs/easyteleports/replacers/DiaryCape.java
+++ src/main/java/com/duckblade/osrs/easyteleports/replacers/DiaryCape.java
@@ -6,6 +6,7 @@ import com.google.common.collect.ImmutableList;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Getter;
+import net.runelite.api.EquipmentInventorySlot;
 import net.runelite.api.gameval.ItemID;
 import net.runelite.api.widgets.Widget;
 
@@ -25,20 +26,35 @@ public class DiaryCape implements Replacer
 		this.enabled = config.enableDiaryCape();
 
 		replacements.clear();
+		replacements.add(new TeleportReplacement("Ardougne", config.replacementArdougne()));
 		replacements.add(new TeleportReplacement("Ardougne: Two-pints", config.replacementArdougne()));
+		replacements.add(new TeleportReplacement("Desert", config.replacementDesert()));
 		replacements.add(new TeleportReplacement("Desert: Jarr", config.replacementDesert()));
+		replacements.add(new TeleportReplacement("Falador", config.replacementFalador()));
 		replacements.add(new TeleportReplacement("Falador: Sir Rebral", config.replacementFalador()));
+		replacements.add(new TeleportReplacement("Fremennik", config.replacementFremennik()));
 		replacements.add(new TeleportReplacement("Fremennik: Thorodin", config.replacementFremennik()));
+		replacements.add(new TeleportReplacement("Kandarin", config.replacementKandarin()));
 		replacements.add(new TeleportReplacement("Kandarin: Flax keeper", config.replacementKandarin()));
+		replacements.add(new TeleportReplacement("Karamja", config.replacementKaramjaJackie()));
 		replacements.add(new TeleportReplacement("Karamja: Pirate Jackie the Fruit", config.replacementKaramjaJackie()));
+		replacements.add(new TeleportReplacement("Karamja (Shilo)", config.replacementKaramjaKaleb()));
 		replacements.add(new TeleportReplacement("Karamja: Kaleb Paramaya (retired)", config.replacementKaramjaKaleb()));
+		replacements.add(new TeleportReplacement("Karamja (Jungle)", config.replacementKaramjaForester()));
 		replacements.add(new TeleportReplacement("Karamja: Jungle forester (retired)", config.replacementKaramjaForester()));
+		replacements.add(new TeleportReplacement("Karamja (Mor Ul Rek)", config.replacementKaramjaTzhaar()));
 		replacements.add(new TeleportReplacement("Karamja: TzHaar-Mej (retired)", config.replacementKaramjaTzhaar()));
+		replacements.add(new TeleportReplacement("Kourend & Kebos", config.replacementKourend()));
 		replacements.add(new TeleportReplacement("Kourend & Kebos: Elise", config.replacementKourend()));
+		replacements.add(new TeleportReplacement("Lumbridge & Draynor", config.replacementLumbridge()));
 		replacements.add(new TeleportReplacement("Lumbridge & Draynor: Hatius Cosaintus", config.replacementLumbridge()));
+		replacements.add(new TeleportReplacement("Morytania", config.replacementMorytania()));
 		replacements.add(new TeleportReplacement("Morytania: Le-sabrè", config.replacementMorytania()));
+		replacements.add(new TeleportReplacement("Varrock", config.replacementVarrock()));
 		replacements.add(new TeleportReplacement("Varrock: Toby", config.replacementVarrock()));
+		replacements.add(new TeleportReplacement("Wilderness", config.replacementWilderness()));
 		replacements.add(new TeleportReplacement("Wilderness: Lesser Fanatic", config.replacementWilderness()));
+		replacements.add(new TeleportReplacement("Western Provinces", config.replacementWestern()));
 		replacements.add(new TeleportReplacement("Western Provinces: Elder Gnome child", config.replacementWestern()));
 		replacements.add(new TeleportReplacement("Twiggy O'Korn", config.replacementTwiggy()));
 	}
@@ -55,6 +71,12 @@ public class DiaryCape implements Replacer
 		return root != null &&
 			root.getText() != null &&
 			ADVENTURE_LOG_HEADER.equals(root.getText());
+	}
+
+	@Override
+	public EquipmentInventorySlot getEquipmentSlot()
+	{
+		return EquipmentInventorySlot.CAPE;
 	}
 
 	@Override
